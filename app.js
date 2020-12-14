f7004839f24602a8f86e3fe5f310aeec9ca5ece7
Johane Chery <73268309+imjohanecher@users.noreply.github.com>
2020-12-13 19:49:59 -0500

    entered missing code for app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
 const usernameErr = username.nextElementSibling
+const email = document.getElementById('email')
+const emailErr = email.nextElementSibling
+const password = document.getElementById('password')
+const passwordErr = password.nextElementSibling
+const password2 = document.getElementById('password2')
+const password2Err = password2.nextElementSibling
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -12,13 +18,30 @@ regForm.addEventListener('click', function (e) {
   // 2. Check if the value is empty
   if (username.value === '') {
     // 3. If empty, give user some feedback
-    usernameErr.className = 'block bg-red-500 text-white'
+    usernameErr.className = 'block text-white bg-red-500'
+  } else {
+    console.log('sending to server')
+    
+  }
+
+  //@TODO: add the validation for email
+  if (email.value === '') {
+    emailErr.className = 'block text-white bg-red-500'
   } else {
     console.log('sending to server')
   }
 
-  //@TODO: add the validation for email
-
   //@TODO: add the validation for password
+  if (password.value === '') {
+    passwordErr.className = 'block text-white bg-red-500'
+  } else {
+    console.log('sending to server')
+  }
+
+  if (password2.value === '') {
+    password2Err.className = 'block text-white bg-red-500'
+  } else {
+    console.log('sending to server')
+  }
 
 })
